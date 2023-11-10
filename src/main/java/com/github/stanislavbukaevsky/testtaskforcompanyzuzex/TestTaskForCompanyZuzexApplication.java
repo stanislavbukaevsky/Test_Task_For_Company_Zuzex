d7a71117ed0185783339b9cd345720e8e4e83ac7
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-11-10 12:08:37 +0300

    Первый коммит

---
+++ src/main/java/com/github/stanislavbukaevsky/testtaskforcompanyzuzex/TestTaskForCompanyZuzexApplication.java
@@ -0,0 +1,13 @@
+package com.github.stanislavbukaevsky.testtaskforcompanyzuzex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForCompanyZuzexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskForCompanyZuzexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/stanislavbukaevsky/testtaskforcompanyzuzex/TestTaskForCompanyZuzexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stanislavbukaevsky.testtaskforcompanyzuzex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskForCompanyZuzexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
